f3fe98c967acdbd8a9f918e42b2f3194b1d93a72
shamil <arsl.sh056@mail.ru>
2021-02-21 11:30:31 +0300

    Adding Discriminator.display_gradients(self) method for controlling non None. Added assertions on input image shapes (256, 256).

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+import torch
 import torch.nn as nn
 
 
@@ -15,7 +16,7 @@ class Discriminator(pl.LightningModule):
         """
 
         :param relu_negative_slope: relu slope, read from config
-        :param verbose: To see output of x.shape when forwarding input
+        :param verbose: To see output of x.shape when forwarding input (only once)
         """
         super(Discriminator, self).__init__()
 
@@ -61,10 +62,17 @@ class Discriminator(pl.LightningModule):
         )
 
     def forward(self, x):
+        """
+        Forward propagation with one-time displaying x shapes transformation (if verbose=True)
+
+        :param x: input tensor
+        :return: processes tensor
+        """
+
         if self.verbose:
             print(f"x shape={x.shape}")
 
-        # assert x.shape[1:] == (3, 256, 256), "Only pictures with shape [-1, 3, 256, 256] are supported"
+        assert x.shape[1:] == torch.Size([3, 256, 256]), "Only pictures with shape [-1, 3, 256, 256] are supported"
 
         x = self.layer1(x)
         if self.verbose:
@@ -90,4 +98,20 @@ class Discriminator(pl.LightningModule):
         if self.verbose:
             print(f"Size of x={x.shape} should be [-1, 1, 16, 16]")
 
+        if self.verbose:
+            # Displaying additional info only once
+            self.verbose = False
+
         return x
+
+    def display_gradients(self):
+        """
+        Displays gradients of each layer just to make sure that all gradients are non-Null, for debugging purposes.
+        :return: None
+        """
+        print("printing gradients of each layer...")
+
+        for i, layer in enumerate([self.layer1, self.layer2, self.layer3, self.layer4, self.layer5, self.layer6]):
+            for j, module in enumerate(layer.parameters()):
+                print(f"Layer: {i}, module: {j}, grad: {module.grad}")
+                assert module.grad is not None, "This gradient in this module is None!"
