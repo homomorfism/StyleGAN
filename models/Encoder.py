ae02da5ef44811fcd05efc73b801e879d9d14b6a
Daniil Igudesman <digudesm@gmail.com>
2021-03-23 12:16:13 +0300

    Update Encoder.py
    fixed indexes

--- models/Encoder.py
+++ models/Encoder.py
@@ -11,35 +11,56 @@ class Encoder(pl.LightningModule):
         model = models.vgg16(pretrained=True)
 
         features = list(model.features.children())[:31:]
-        self.block1 = nn.Sequential(*features[:5:])
-        self.pooling1 = nn.MaxPool2d(
-            kernel_size=(2, 2), stride=(8, 8), dilation=(1, 1), ceil_mode=False, return_indices=True
+        self.block1 = nn.Sequential(*features[:4:])
+        self.next_pooling1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False, return_indices=True)
+        self.cat_pooling1 = nn.MaxPool2d(
+            kernel_size=(2, 2), stride=(8, 8), dilation=(1, 1), ceil_mode=False
         )
 
-        self.block2 = nn.Sequential(*features[5:10:])
-        self.pooling2 = nn.MaxPool2d(
-            kernel_size=(2, 2), stride=(4, 4), dilation=(1, 1), ceil_mode=False, return_indices=True
+        self.block2 = nn.Sequential(*features[5:9:])
+        self.next_pooling2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False, return_indices=True)
+        self.cat_pooling2 = nn.MaxPool2d(
+            kernel_size=(2, 2), stride=(4, 4), dilation=(1, 1), ceil_mode=False
         )
 
-        self.block3 = nn.Sequential(*features[10:17:])
-        self.pooling3 = nn.MaxPool2d(
-            kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1), ceil_mode=False, return_indices=True
+        self.block3 = nn.Sequential(*features[10:16:])
+        self.next_pooling3 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False, return_indices=True)
+        self.cat_pooling3 = nn.MaxPool2d(
+            kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1), ceil_mode=False
         )
 
-        self.block4 = nn.Sequential(*features[17:24:])
+        self.block4 = nn.Sequential(*features[17:23:])
+        # self.next_pooling4 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False, return_indices=True)
 
     def forward(self, x):
 
         x = self.block1(x)
-        to_concat1, indexes1 = self.pooling1(x)
+        to_concat1 = self.cat_pooling1(x)
+        print('to_concat1.shape: ', to_concat1.shape)
 
+        x, indexes1 = self.next_pooling1(x)
+        print('indexes1.shape: ', x.shape)
+
+        
         x = self.block2(x)
-        to_concat2, indexes2 = self.pooling2(x)
+        to_concat2 = self.cat_pooling2(x)
+        print('to_concat2.shape: ', to_concat2.shape)
+
+        x, indexes2 = self.next_pooling2(x)
+        print('indexes2.shape: ', x.shape)
+
 
         x = self.block3(x)
-        to_concat3, indexes3 = self.pooling3(x)
+        to_concat3 = self.cat_pooling3(x)
+        print('to_concat3.shape: ', to_concat3.shape)
+
+        x, indexes3 = self.next_pooling3(x)
+        print('indexes3.shape: ', x.shape)
 
         x = self.block4(x)
+        # x, indexes4 = self.next_pooling4(x)
+        # print('indexes4.shape: ', x.shape)
 
-        output = torch.cat((to_concat1, to_concat2, to_concat3, x), dim=2)
+
+        output = torch.cat((to_concat1, to_concat2, to_concat3, x), dim=1)
         return output, [indexes1, indexes2, indexes3]
